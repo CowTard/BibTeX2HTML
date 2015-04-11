c804a0a6f031d496afece84b9275c903372b84c2
Joao Costa <sd_joao@hotmail.com>
2015-04-11 18:39:57 +0100

    construtores incompleto

--- src/elementos/Article.java
+++ src/elementos/Article.java
@@ -1,5 +1,19 @@
 package elementos;
 
 public class Article extends Entry {
-
+	String author, title, journal, year, volume, number, pages, month, note, key;
+	
+	Article(String author, String title, String journal, String year, String volume, String number, String pages, String month, String note, String key) {
+		this.author = author;
+		this.title = title;
+		this.journal = journal;
+		this.year = year;
+		this.volume = volume;
+		this.number = number;
+		this.pages = pages;
+		this.month = month;
+		this.note = note;
+		this.key = key;
+	}
 }
+
--- src/elementos/Book.java
+++ src/elementos/Book.java
@@ -1,5 +1,30 @@
 package elementos;
 
 public class Book extends Entry {
-
+	String author, editor, title, publisher, year, volume, number, series, address, edition, month, note, key;
+	
+	Book(String type, String authorEditor, String title, String publisher, String year, String... optionals) {
+		if (type.equals("author"))
+			author = authorEditor;
+		else if (type.equals("editor"))
+			this.editor = authorEditor;
+		else throw new IllegalArgumentException();
+		
+		this.title = title;
+		this.publisher = publisher;
+		this.year = year;
+		
+		if (optionals[0].equals("volume"))
+			author = authorEditor;
+		else if (optionals[1].equals("number"))
+			this.editor = authorEditor;
+		else throw new IllegalArgumentException();
+		
+		series = optionals[2];
+		address = optionals[3];
+		edition = optionals[4];
+		month = optionals[5];
+		note = optionals[6];
+		key = optionals[7];
+	}
 }
