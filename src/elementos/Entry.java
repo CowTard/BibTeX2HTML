b06522213252693bc24a8c52ee44c6822f614c01
Joao Costa <sd_joao@hotmail.com>
2015-06-09 13:49:11 +0100

    article changes

--- src/elementos/Entry.java
+++ src/elementos/Entry.java
@@ -1,11 +1,19 @@
 package elementos;
 
-public class Entry {
+public abstract class Entry {
 	protected String reference;
 	
-	public Entry () {}
-	
-	public void setReference(String reference){
+	public void setReference(String reference) {
 		this.reference = reference;
 	}
+	
+	public String toString() {
+		String output = "";
+		output += "<p style=\"margin-left: 20px; text-indent: -20px;\">";
+		output += printContents();
+		output += "</p>";
+		return output;
+	}
+	
+	public abstract String printContents();
 }
