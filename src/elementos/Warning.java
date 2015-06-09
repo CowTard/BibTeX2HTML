c3ebd432f2c9becd94292e032910512271185610
Joao Costa <sd_joao@hotmail.com>
2015-06-09 19:21:25 +0100

    class w

---
+++ src/elementos/Warning.java
@@ -0,0 +1,15 @@
+package elementos;
+
+public class Warning {
+	private String alert;
+	private int lineNumber;
+	
+	public Warning() {
+		
+	}
+	
+	public String toString(){
+	String toreturn = null;
+	return toreturn;
+	}
+}
